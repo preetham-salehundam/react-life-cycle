fa1bb296c1f2acd942f157b3f643823be8dd9b3f
502554557 <preetham.salehundam@ge.com>
2017-05-24 16:13:04 +0530

    life cycke methods added

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -7,11 +7,19 @@ class TodoList extends React.Component{
         console.log("1 - constructor is invoked")
     }
     componentWillMount(){
-            console.log("get intial state life hook - we can set intial state of the Component");
+            console.log("2- componentWillMount invoked before render method is called and after constructor is called");
     }
    
     render(){
+        console.log("3- render is invoked after componentWillMount method")
         return(<ul><li>Test</li></ul>);
+    }
+    componentDidMount(){
+        console.log("4- componentDidMount will be invoked after render method")
+    }
+    shouldComponentUpdate(){
+        console.log("5- shouldComponentUpdate returns boolean and render is called if true is returned ")
+        return true;
     }
 
 } 
